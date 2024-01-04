0387453c82cec04ac9d8e0e431350d10b33a78b4
Chideraa <chideraaozobu@gmail.com>
2024-01-04 18:37:42 +0200

    task 5

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -11,12 +11,8 @@
 
 int binary_tree_is_root(const binary_tree_t *node)
 {
-	if (node == NULL)
+	if (node == NULL || node->parent != NULL)
 		return (0);
-	if (node != NULL)
-	{
-		if (node->parent == NULL)
-			return (1);
-		return (0);
-	}
+
+	return 1;
 }
