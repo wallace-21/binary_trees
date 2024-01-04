4f114e7e0bd75567b32351520e4038f0cb2d0953
Chideraa <chideraaozobu@gmail.com>
2024-01-04 16:35:59 +0200

    task 1

---
+++ 1-binary_tree_insert_left.c
@@ -0,0 +1,33 @@
+#include "binary_trees.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *
+ * binary_tree_insert_left - must return a pointer to the created node
+ * @parent - parent node
+ * @value - value to store in the node
+ *
+ * Return:  pointer to the created node, or NULL on failure or if parent is NULL
+ */
+
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+	binary_tree_t *copy;
+
+	if (parent == NULL)
+		return (NULL);
+
+	copy = binary_tree_node(parent, value);
+	if (copy == NULL)
+		return (NULL);
+
+	if (parent->left != NULL)
+	{
+		copy->left = parent->left;
+		parent->left->parent = copy;
+	}
+	parent->left = copy;
+
+	return (copy);
+}
