36f2f35b91db0138dc19dec31355911431196438
Chideraa <chideraaozobu@gmail.com>
2024-01-05 03:25:50 +0200

    t13

---
+++ 13-binary_tree_nodes.c
@@ -0,0 +1,27 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_nodes - Counts the nodes 1 or more
+ * @tree: pointer to root node of the tree to count no of nodes.
+ * Return: 0 if tree is NULL.
+ */
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+	size_t size = 0;
+
+	if (tree)
+	{
+		if (tree->left || tree->right)
+		{
+			size += 1;
+		}
+		else
+		{
+			size += 0;
+		}
+		size += binary_tree_nodes(tree->left);
+		size += binary_tree_nodes(tree->right);
+	}
+
+	return (size);
+}
